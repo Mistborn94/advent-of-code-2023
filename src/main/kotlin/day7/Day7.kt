23a6fa2bd68f26805d4912738da25e696c67ac64
Renette Ros <supergirl0894+git@gmail.com>
2023-12-07 16:46:58 +0200

    Clean up day 7 a little bit

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -11,28 +11,30 @@ val twoPair = { hand: Map<Char, Int> -> hand.size == 3 && hand.values.max() == 2
 val pair = { hand: Map<Char, Int> -> hand.size == 4 && hand.values.max() == 2 }
 val highCard = { hand: Map<Char, Int> -> hand.size == 5 }
 
-
-val typesA = listOf(fiveOfAKind, fourOfAKind, fullHouse, threeOfAKind, twoPair, pair, highCard)
+val handTypes = listOf(fiveOfAKind, fourOfAKind, fullHouse, threeOfAKind, twoPair, pair, highCard)
 
 val cardValuesA = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
 val cardValuesB = listOf('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
-fun solveA(text: String, debug: Debug = Debug.Disabled): Int {
+
+fun solveA(text: String, debug: Debug = Debug.Disabled): Int = solve(text, false)
+fun solveB(text: String, debug: Debug = Debug.Disabled): Int = solve(text, true)
+
+private fun solve(text: String, part2: Boolean): Int {
     val hands = text.lines().map { line ->
         val (cards, bid) = line.split(' ')
 
-        Hand(cards, bid.toInt(), false)
+        Hand(cards, bid.toInt(), part2)
     }.sorted()
 
     return hands.mapIndexed { index, hand ->
         val rank = hands.size - index
         rank * hand.bid
     }.sum()
-
 }
 
 class Hand(private val cards: String, val bid: Int, private val part2: Boolean) : Comparable<Hand> {
     private val cardCounts = cards.groupBy { it }.mapValues { (_, v) -> v.size }
-    private val typeA = typesA.indexOfFirst { it(cardCounts) }
+    private val typeA = handTypes.indexOfFirst { it(cardCounts) }
     private val typeB = bestTypeWithJoker()
 
     private val type: Int
@@ -55,34 +57,16 @@ class Hand(private val cards: String, val bid: Int, private val part2: Boolean) 
 
     //Returns zero if this object is equal to the specified other object, a negative number if it's less than other, or a positive number if it's greater than other.
     override fun compareTo(other: Hand): Int {
+        val typeCompare = this.type.compareTo(other.type)
         return if (this.cards == other.cards) {
             0
-        } else if (this.type < other.type) {
-            -1
-        } else if (this.type > other.type) {
-            1
+        } else if (typeCompare != 0) {
+            typeCompare
         } else {
             val (a, b) = cardRanks.zip(other.cardRanks).first { (a, b) -> a != b }
             a.compareTo(b)
         }
     }
 
-    override fun toString(): String {
-        return cards
-    }
-
-
-}
-
-fun solveB(text: String, debug: Debug = Debug.Disabled): Int {
-    val hands = text.lines().map { line ->
-        val (cards, bid) = line.split(' ')
-
-        Hand(cards, bid.toInt(), true)
-    }.sorted()
-
-    return hands.mapIndexed { index, hand ->
-        val rank = hands.size - index
-        rank * hand.bid
-    }.sum()
+    override fun toString(): String = cards
 }
