54c33654a9733bbc427ac3affbbbbb92636d7702
Renette Ros <supergirl0894+git@gmail.com>
2023-12-17 14:42:44 +0200

    Optimize day 17
    Eliminate double search by adding a special neighbour condition for the starting state

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -7,7 +7,6 @@ import helper.point.Direction
 import helper.point.Point
 import helper.point.contains
 import helper.point.get
-import kotlin.math.min
 
 data class PointInDirection(val point: Point, val direction: Direction, val line: Int) {
     fun neighbours(): List<PointInDirection> {
@@ -25,7 +24,8 @@ data class PointInDirection(val point: Point, val direction: Direction, val line
             if (line < 10) {
                 add(PointInDirection(point + direction.pointPositiveDown, direction, line + 1))
             }
-            if (line >= 4) {
+            //line == 0 caters for the starting state where we can go any direction
+            if (line >= 4 || line == 0) {
                 add(PointInDirection(point + direction.right.pointPositiveDown, direction.right, 1))
                 add(PointInDirection(point + direction.left.pointPositiveDown, direction.left, 1))
             }
@@ -49,21 +49,14 @@ fun solveA(text: String, debug: Debug = Debug.Disabled): Int {
 
 fun solveB(text: String, debug: Debug = Debug.Disabled): Int {
     val map = text.lines().map { line -> line.map { it.digitToInt() } }
-    val pathA = searchWithStartDirection(map, Direction.DOWN)
-    val pathB = searchWithStartDirection(map,  Direction.RIGHT)
-    return min(pathA.getScore(), pathB.getScore())
-}
-
-private fun searchWithStartDirection(
-    map: List<List<Int>>,
-    startDirection: Direction
-): GraphSearchResult<PointInDirection> {
-    val start = PointInDirection(Point(0, 0), startDirection, 0)
+    val start = PointInDirection(Point(0, 0), Direction.RIGHT, 0)
     val end = Point(map[0].lastIndex, map.lastIndex)
-    return findShortestPathByPredicate(
+    val path = findShortestPathByPredicate(
         start,
         { (p, _, line) -> p == end && line >= 4 },
         { it.ultraNeighbours().filter { (p) -> p in map } },
         { _, (point) -> map[point] }
     )
+    return path.getScore()
 }
+
